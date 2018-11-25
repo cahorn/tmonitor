c6c8add73ed5729a50eaae94aedf4c718482dfe2
C. Ansel Horn <ansel@horn.name>
2018-11-25 15:19:39 -0500

    Patch temper module to be more backwards-compatible

--- tmonitor/temper.py
+++ tmonitor/temper.py
@@ -5,7 +5,7 @@ def query():
     Query the attached TEMPer device via temper-query command and parse the
     result.
     """
-    p = subprocess.run(['/usr/local/bin/temper_query'], capture_output=True)
+    p = subprocess.run(['/usr/local/bin/temper_query'], stdout=subprocess.PIPE)
     p.check_returncode()
     temp = float(p.stdout.decode(encoding='ascii').strip())
     return temp
