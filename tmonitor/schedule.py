d195800ed450b03b5c60c9b97f5160bf4d94626b
C. Ansel Horn <ansel@horn.name>
2018-11-26 16:35:36 +0000

    Update scheduler to run under default raspbian vixie cron

--- tmonitor/schedule.py
+++ tmonitor/schedule.py
@@ -7,12 +7,13 @@ cron = crontab.CronTab(user=True)
 def add(args):
     """Add a new monitor poll to the schedule."""
     # Construct monitor poll command
-    cmd_args = ['tmonitor']
+    cmd_args = ['/usr/local/bin/tmonitor']
     for flag, arg in args.items():
         if flag not in ['boot', 'hourly', 'daily', 'weekly', 'list'] \
                 and arg is not None:
             cmd_args.append("--{0}".format(flag))
             cmd_args.append("'{0}'".format(arg))
+    cmd_args.append('| logger -t tmonitor')
     command = " ".join(cmd_args)
     # Construct an identifiable comment
     comment = "[tmonitor] {0}".format(datetime.datetime.now())
